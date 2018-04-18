adbb94454cdec23166d0c0f9c75a2d55254342c6
maciej.menakiewicz <maciej.menakiewicz@allegro.pl>
2018-04-18 14:24:06 +0200

    Simple server with hello world message and another one returning user-agent header

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,11 @@
 //Twój kod
+const http = require("http");
+
+const srv = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end("Witaj świecie, z backendu!");
+});
+
+srv.listen(3000, () => {
+    console.log("server listening on localhost:3000");
+});
--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,12 @@
 //Twój kod
+const http = require("http");
+
+const srv = http.createServer((req, res) => {
+    let agent = req.headers['user-agent'];
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end(agent);
+});
+
+srv.listen(3000, ()=> {
+    console.log("server on localhost:3000");
+});
